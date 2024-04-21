2925fd99c842fdaf6e35c812f84aa06f3ec91053
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-04-21 21:48:21 +0700

    chore: add `nanoid`

--- src/pages/v1.tsx
+++ src/pages/v1.tsx
@@ -2,8 +2,8 @@ import { useAtom } from "jotai";
 import { DragDropContext, Draggable, DropResult } from "react-beautiful-dnd";
 import StrictModeDroppable from "../components/StrictModeDroppable";
 import TaskItem from "../components/TaskItem";
-import { dataAtom } from "../store";
 import { tw } from "../lib/utils/tw";
+import { dataAtom } from "../store";
 
 export default function VersionOne() {
   const [data, setData] = useAtom(dataAtom);
@@ -77,21 +77,27 @@ export default function VersionOne() {
                       {...provided.droppableProps}
                       ref={provided.innerRef}
                     >
-                      {item.tasks.map((task, index) => (
-                        <Draggable
-                          key={task.id}
-                          draggableId={task.id}
-                          index={index}
-                        >
-                          {(provided) => (
-                            <TaskItem
-                              provided={provided}
-                              progress={task.progress}
-                              name={task.name}
-                            />
-                          )}
-                        </Draggable>
-                      ))}
+                      {item.tasks.length ? (
+                        item.tasks.map((task, index) => (
+                          <Draggable
+                            key={task.id}
+                            draggableId={task.id}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <TaskItem
+                                provided={provided}
+                                progress={task.progress}
+                                name={task.name}
+                              />
+                            )}
+                          </Draggable>
+                        ))
+                      ) : (
+                        <div className="bg-[#FAFAFA] w-full rounded-md border-2 border-[#E0E0E0] py-3 px-4">
+                          <p className="text-sm text-[#757575]">No Task</p>
+                        </div>
+                      )}
                       {provided.placeholder}
                     </div>
                   )}
@@ -109,187 +115,5 @@ export default function VersionOne() {
         </section>
       </main>
     </>
-  );
-}
-
-function GroupTaskOne() {
-  const [data, setData] = useAtom(dataAtom);
-
-  function handleOnDragEnd(results: DropResult) {
-    if (!results.destination) return;
-
-    const items = Array.from(data);
-    const [reorderedItem] = items.splice(results.source.index, 1);
-    items.splice(results.destination.index, 0, reorderedItem);
-
-    setData(items);
-  }
-
-  return (
-    <div className="rounded-md p-3 w-[326px] bg-primary-surface border-2 border-primary-border">
-      <div className="space-y-2">
-        <button
-          type="button"
-          className="border-primary-border border-2 rounded-md py-1 px-3 text-primary-main text-xs"
-        >
-          Group Task 1
-        </button>
-        <p className="font-bold text-xs">January - March</p>
-      </div>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <StrictModeDroppable droppableId="task-1">
-          {(provided) => (
-            <div
-              className="flex flex-col space-y-4 my-2 justify-center items-start w-full"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {data.map((item, index) => (
-                <Draggable
-                  key={item.id.toString()}
-                  draggableId={item.id.toString()}
-                  index={index}
-                >
-                  {(provided) => (
-                    <TaskItem
-                      provided={provided}
-                      name={item.name}
-                      progress={item.progress}
-                    />
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </StrictModeDroppable>
-      </DragDropContext>
-      <button
-        type="button"
-        className="flex justify-center items-center space-x-2"
-      >
-        <img src="/images/plus.svg" alt="plus" />
-        <span className="text-xs">New Task</span>
-      </button>
-    </div>
-  );
-}
-
-function GroupTaskTwo() {
-  return (
-    <div className="border-2 rounded-md p-3 w-[326px] border-secondary-border bg-secondary-surface">
-      <div className="space-y-2">
-        <button
-          type="button"
-          className="border-secondary-border border-2 rounded-md py-1 px-3 text-secondary-pressed text-xs"
-        >
-          Group Task 2
-        </button>
-        <p className="font-bold text-xs">April - June</p>
-      </div>
-      <div className="bg-[#FAFAFA] my-2 w-full text-[#757575] border-2 px-4 py-3 rounded-md border-[#E0E0E0]">
-        <p className="text-sm">No Task</p>
-      </div>
-      <button
-        type="button"
-        className="flex justify-center items-center space-x-2"
-      >
-        <img src="/images/plus.svg" alt="plus" />
-        <span className="text-xs">New Task</span>
-      </button>
-    </div>
-  );
-}
-
-function GroupTaskThree() {
-  function handleOnDragEnd() {}
-
-  return (
-    <div className="rounded-md p-3 w-[326px] bg-danger-surface border-2 border-danger-border">
-      <div className="space-y-2">
-        <button
-          type="button"
-          className="border-danger-border border-2 rounded-md py-1 px-3 text-danger-main text-xs"
-        >
-          Group Task 3
-        </button>
-        <p className="font-bold text-xs">July - September</p>
-      </div>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <StrictModeDroppable droppableId="ROOT">
-          {(provided) => (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className="flex flex-col space-y-4 my-2 justify-center items-start w-full"
-            >
-              <Draggable key="task-3" draggableId="task-3" index={1}>
-                {(provided) => (
-                  <TaskItem
-                    provided={provided}
-                    name="Data Migration: Performance & Culture End Game"
-                    progress="60%"
-                  />
-                )}
-              </Draggable>
-              {provided.placeholder}
-            </div>
-          )}
-        </StrictModeDroppable>
-      </DragDropContext>
-      <button
-        type="button"
-        className="flex justify-center items-center space-x-2"
-      >
-        <img src="/images/plus.svg" alt="plus" />
-        <span className="text-xs">New Task</span>
-      </button>
-    </div>
-  );
-}
-
-function GroupTaskFour() {
-  function handleOnDragEnd() {}
-
-  return (
-    <div className="rounded-md p-3 w-[326px] bg-success-surface border-2 border-success-border">
-      <div className="space-y-2">
-        <button
-          type="button"
-          className="border-success-border border-2 rounded-md py-1 px-3 text-success-main text-xs"
-        >
-          Group Task 4
-        </button>
-        <p className="font-bold text-xs">October - December</p>
-      </div>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <StrictModeDroppable droppableId="ROOT">
-          {(provided) => (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className="flex flex-col space-y-4 my-2 justify-center items-start w-full"
-            >
-              <Draggable key="task-4" draggableId="task-4" index={1}>
-                {(provided) => (
-                  <TaskItem
-                    provided={provided}
-                    name="Bundle interplanetary analytics for improved transmission"
-                    progress="20%"
-                  />
-                )}
-              </Draggable>
-            </div>
-          )}
-        </StrictModeDroppable>
-      </DragDropContext>
-      <button
-        type="button"
-        className="flex justify-center items-center space-x-2"
-      >
-        <img src="/images/plus.svg" alt="plus" />
-        <span className="text-xs">New Task</span>
-      </button>
-    </div>
   );
 }
