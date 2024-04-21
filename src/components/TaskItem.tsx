b5506b650d13718fa0d6ecd9a3914defb46a360d
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-04-21 17:00:33 +0700

    feat: create component `TaskItem`

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,58 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+import { DraggableProvided } from "react-beautiful-dnd";
+import { tw } from "../lib/utils/tw";
+
+type TaskItemProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> & {
+  provided: DraggableProvided;
+  name: string;
+  progress: string;
+};
+
+export default function TaskItem({ provided, name, progress }: TaskItemProps) {
+  return (
+    <>
+      <div
+        {...provided.dragHandleProps}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        className="bg-[#FAFAFA] w-fit rounded-md border-2 border-[#E0E0E0] p-4"
+      >
+        <div className="border-b-2 border-dashed border-[#E0E0E0] py-1.5">
+          <p className="font-bold text-sm">{name}</p>
+        </div>
+        <div className="py-4 flex w-full justify-between items-center">
+          <div className="flex justify-center items-center space-x-2">
+            <div className="w-[201px] bg-[#EDEDED] rounded-full overflow-hidden h-5">
+              <div
+                style={{ width: progress }}
+                className={tw(
+                  "h-5 rounded-full rounded-r-none",
+                  progress === "100%" ? "bg-success-main" : "bg-primary-main"
+                )}
+              ></div>
+            </div>
+            {progress === "100%" ? (
+              <img src="/images/checklist.svg" alt="checklist" />
+            ) : (
+              <span className="text-xs text-[#757575]">{progress}</span>
+            )}
+          </div>
+          <button type="button">
+            <img src="/images/more.svg" alt="more" />
+          </button>
+        </div>
+      </div>
+      <div className="fixed top-0 drop-shadow-lg rounded-lg p-4">
+        <ul className="flex flex-col justify-center items-center space-y-5">
+          <li className="flex justify-center items-center w-fit space-x-4">
+            <img src="" alt="move right" />
+            <span>Move Right</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
--- src/pages/v1.tsx
+++ src/pages/v1.tsx
@@ -4,25 +4,24 @@ import {
   Draggable,
   Droppable,
   DroppableProps,
+  OnDragEndResponder,
 } from "react-beautiful-dnd";
-import { tw } from "../lib/utils/tw";
+import TaskItem from "../components/TaskItem";
 
 const data = [
   {
     id: "1",
     name: "Re-designed the zero-g doggie bags. No more spills!",
+    progress: "100%",
   },
   {
     id: "2",
     name: "Bundle interplanetary analytics for improved transmission",
-  },
-  {
-    id: "3",
-    name: "mantap dan luar biasa",
+    progress: "30%",
   },
 ];
 
-export const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
+export function StrictModeDroppable({ children, ...props }: DroppableProps) {
   const [enabled, setEnabled] = useState(false);
 
   useEffect(() => {
@@ -39,30 +38,19 @@ export const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
   }
 
   return <Droppable {...props}>{children}</Droppable>;
-};
+}
 
 export default function VersionOne() {
   const [characters, updateCharacters] = useState(data);
 
   function handleOnDragEnd(results) {
-    const { source, destination, type } = results;
+    if (!results.destination) return;
 
-    if (!destination) return;
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(results.source.index, 1);
+    items.splice(results.destination.index, 0, reorderedItem);
 
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
-
-    if (type === "group") {
-      const reorderedCharacters = [...characters];
-
-      const [removedCharacters] = reorderedCharacters.splice(source.index, 1);
-      reorderedCharacters.splice(destination.index, 0, removedCharacters);
-
-      return updateCharacters(reorderedCharacters);
-    }
+    updateCharacters(items);
   }
 
   return (
@@ -82,7 +70,8 @@ export default function VersionOne() {
       </header>
       <main className="w-full py-4 flex justify-center items-start min-h-screen">
         <section className="flex max-w-7xl w-full space-x-4 justify-start items-start">
-          <div className="rounded-md p-3 w-[326px] bg-primary-surface border-2 border-primary-main">
+          {/* Task 1 */}
+          <div className="rounded-md p-3 w-[326px] bg-primary-surface border-2 border-primary-border">
             <div className="space-y-2">
               <button
                 type="button"
@@ -93,10 +82,10 @@ export default function VersionOne() {
               <p className="font-bold text-xs">January - March</p>
             </div>
             <DragDropContext onDragEnd={handleOnDragEnd}>
-              <Droppable droppableId="ROOT" type="group">
+              <StrictModeDroppable droppableId="task-1">
                 {(provided) => (
                   <div
-                    className="flex flex-col list space-y-4 my-2 justify-center items-start w-full"
+                    className="flex flex-col space-y-4 my-2 justify-center items-start w-full"
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                   >
@@ -104,43 +93,21 @@ export default function VersionOne() {
                       <Draggable
                         key={item.id.toString()}
                         draggableId={item.id.toString()}
-                        index={index + 1}
+                        index={index}
                       >
                         {(provided) => (
-                          <div
-                            {...provided.dragHandleProps}
-                            {...provided.draggableProps}
-                            ref={provided.innerRef}
-                            className="bg-[#FAFAFA] w-fit rounded-md border-2 border-[#E0E0E0] p-4"
-                          >
-                            <div className="border-b-2 border-dashed border-[#E0E0E0] py-1.5">
-                              <p className="font-bold text-sm">{item.name}</p>
-                            </div>
-                            <div className="py-4 flex w-full justify-between items-center">
-                              <div className="flex justify-center items-center space-x-2">
-                                <div className="w-[201px] bg-[#EDEDED] rounded-full overflow-hidden h-5">
-                                  <div
-                                    className={tw(
-                                      `w-1/2 h-5 rounded-full rounded-r-none bg-primary-main`
-                                    )}
-                                  ></div>
-                                </div>
-                                <span className="text-xs text-[#757575]">
-                                  30%
-                                </span>
-                              </div>
-                              <button type="button">
-                                <img src="/images/more.svg" alt="more" />
-                              </button>
-                            </div>
-                          </div>
+                          <TaskItem
+                            provided={provided}
+                            name={item.name}
+                            progress={item.progress}
+                          />
                         )}
                       </Draggable>
                     ))}
                     {provided.placeholder}
                   </div>
                 )}
-              </Droppable>
+              </StrictModeDroppable>
             </DragDropContext>
             <button
               type="button"
@@ -150,6 +117,7 @@ export default function VersionOne() {
               <span className="text-xs">New Task</span>
             </button>
           </div>
+          {/* Task 2 */}
           <div className="border-2 rounded-md p-3 w-[326px] border-secondary-border bg-secondary-surface">
             <div className="space-y-2">
               <button
@@ -171,49 +139,38 @@ export default function VersionOne() {
               <span className="text-xs">New Task</span>
             </button>
           </div>
-          <div className="rounded-md p-3 w-[326px] bg-primary-surface border-2 border-primary-main">
+          {/* Task 3 */}
+          <div className="rounded-md p-3 w-[326px] bg-danger-surface border-2 border-danger-border">
             <div className="space-y-2">
               <button
                 type="button"
-                className="border-primary-border border-2 rounded-md py-1 px-3 text-primary-main text-xs"
+                className="border-danger-border border-2 rounded-md py-1 px-3 text-danger-main text-xs"
               >
                 Group Task 3
               </button>
               <p className="font-bold text-xs">July - September</p>
             </div>
             <DragDropContext>
-              <Droppable droppableId="ROOT" type="group">
+              <StrictModeDroppable droppableId="ROOT">
                 {(provided) => (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                     className="flex flex-col space-y-4 my-2 justify-center items-start w-full"
                   >
-                    <div className="bg-[#FAFAFA] w-fit rounded-md border-2 border-[#E0E0E0] p-4">
-                      <div className="border-b-2 border-dashed border-[#E0E0E0] py-1.5">
-                        <p className="font-bold text-sm">
-                          Data Migration: Performance & Culture End Game
-                        </p>
-                      </div>
-                      <div className="py-4 flex w-full justify-between items-center">
-                        <div className="flex justify-center items-center space-x-2">
-                          <div className="w-[201px] bg-[#EDEDED] rounded-full overflow-hidden h-5">
-                            <div
-                              className={tw(
-                                `h-5 w-3/5 rounded-full rounded-r-none bg-primary-main`
-                              )}
-                            ></div>
-                          </div>
-                          <span className="text-xs text-[#757575]">60%</span>
-                        </div>
-                        <button type="button">
-                          <img src="/images/more.svg" alt="more" />
-                        </button>
-                      </div>
-                    </div>
+                    <Draggable key="7" draggableId="7" index={1}>
+                      {(provided) => (
+                        <TaskItem
+                          provided={provided}
+                          name="Data Migration: Performance & Culture End Game"
+                          progress="60%"
+                        />
+                      )}
+                    </Draggable>
+                    {provided.placeholder}
                   </div>
                 )}
-              </Droppable>
+              </StrictModeDroppable>
             </DragDropContext>
             <button
               type="button"
@@ -223,40 +180,38 @@ export default function VersionOne() {
               <span className="text-xs">New Task</span>
             </button>
           </div>
-          <div className="rounded-md p-3 w-[326px] bg-primary-surface border-2 border-primary-main">
+          {/* Task 4 */}
+          <div className="rounded-md p-3 w-[326px] bg-success-surface border-2 border-success-border">
             <div className="space-y-2">
               <button
                 type="button"
-                className="border-primary-border border-2 rounded-md py-1 px-3 text-primary-main text-xs"
+                className="border-success-border border-2 rounded-md py-1 px-3 text-success-main text-xs"
               >
-                Group Task 1
+                Group Task 4
               </button>
               <p className="font-bold text-xs">January - March</p>
             </div>
-            <div className="flex flex-col space-y-4 my-2 justify-center items-start w-full">
-              <div className="bg-[#FAFAFA] w-fit rounded-md border-2 border-[#E0E0E0] p-4">
-                <div className="border-b-2 border-dashed border-[#E0E0E0] py-1.5">
-                  <p className="font-bold text-sm">
-                    Bundle interplanetary analytics for improved transmission
-                  </p>
-                </div>
-                <div className="py-4 flex w-full justify-between items-center">
-                  <div className="flex justify-center items-center space-x-2">
-                    <div className="w-[201px] bg-[#EDEDED] rounded-full overflow-hidden h-5">
-                      <div
-                        className={tw(
-                          `h-5 w-1/5 rounded-full rounded-r-none bg-primary-main`
-                        )}
-                      ></div>
-                    </div>
-                    <span className="text-xs text-[#757575]">30%</span>
+            <DragDropContext>
+              <StrictModeDroppable droppableId="task-4">
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="flex flex-col space-y-4 my-2 justify-center items-start w-full"
+                  >
+                    <Draggable key="task-4" draggableId="task-4" index={1}>
+                      {(provided) => (
+                        <TaskItem
+                          provided={provided}
+                          name="Bundle interplanetary analytics for improved transmission"
+                          progress="20%"
+                        />
+                      )}
+                    </Draggable>
                   </div>
-                  <button type="button">
-                    <img src="/images/more.svg" alt="more" />
-                  </button>
-                </div>
-              </div>
-            </div>
+                )}
+              </StrictModeDroppable>
+            </DragDropContext>
             <button
               type="button"
               className="flex justify-center items-center space-x-2"
